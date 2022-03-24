e6043bc76409651701e87579fefbd483c991585b
Michael Scalzetti <maestromike5@gmail.com>
2022-03-24 19:15:56 -0400

    added autohide battery to extension list

--- finalize.sh
+++ finalize.sh
@@ -6,10 +6,11 @@ usermod --shell /bin/zsh $USER
 
 # List gnome extensions to install
 echo -e "\nGnome extensions:"
-echo -e "\AppIndicator and KStatusNotifierItem Support: https://extensions.gnome.org/extension/615/appindicator-support/"
+echo -e "\tAppIndicator and KStatusNotifierItem Support: https://extensions.gnome.org/extension/615/appindicator-support/"
 echo -e "\tArcMenu: https://extensions.gnome.org/extension/3628/arcmenu/"
 echo -e "\tLaunch new instance: https://extensions.gnome.org/extension/600/launch-new-instance/"
 echo -e "\tRemovable Drive Menu: https://extensions.gnome.org/extension/7/removable-drive-menu/"
 echo -e "\tTactile: https://extensions.gnome.org/extension/4548/tactile/"
 echo -e "\tVitals: https://extensions.gnome.org/extension/1460/vitals/"
 echo -e "\tWorkspace Indicator: https://extensions.gnome.org/extension/21/workspace-indicator/"
+echo -e "\thttps://extensions.gnome.org/extension/595/autohide-battery/"
