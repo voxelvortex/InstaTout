62e1ca550abb9334874d1a4849efc9e2f26d416f
Michael Scalzetti <maestromike5@gmail.com>
2022-03-24 01:52:07 -0400

    adding auto-linking for zshrc and neofetch

--- auto_move.sh
+++ auto_move.sh
@@ -4,3 +4,8 @@ USER=$2
 7z x $1.zip -o./$2/
 
 mv ./$2/* /home/$2/
+
+chown -R $USER /home/$USER
+chgrp -R $USER /home/$USER
+ln -s /home/$USER/.zshrc ~/.zshrc
+ln -s /home/$USER/.config/neofetch/config.conf ~/.config/neofetch/config.conf
