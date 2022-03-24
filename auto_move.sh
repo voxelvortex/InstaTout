e2a8f8ed8ed6484eedd73153b0697e6b52740a11
Michael Scalzetti <maestromike5@gmail.com>
2022-03-24 02:13:22 -0400

    added link for .gitconfig

--- auto_move.sh
+++ auto_move.sh
@@ -9,3 +9,4 @@ chown -R $USER /home/$USER
 chgrp -R $USER /home/$USER
 ln -s /home/$USER/.zshrc ~/.zshrc
 ln -s /home/$USER/.config/neofetch/config.conf ~/.config/neofetch/config.conf
+ln -s /home/$USER/.gitconfig ~/.gitconfig
