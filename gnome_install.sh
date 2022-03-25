4f9894d66f89334211af76f90734befcb4129da2
Michael Scalzetti <maestromike5@gmail.com>
2022-03-24 20:57:53 -0400

    adding a command that disables seperate workspaces per monitor. I dont know why that setting is default, its stupid.

--- gnome_install.sh
+++ gnome_install.sh
@@ -6,4 +6,6 @@ cp ./files/preferences /etc/apt/preferences
 apt-get update &&\
 apt -t unstable install -y gnome-session gnome-shell gnome-backgrounds gnome-applets gnome-control-center mutter gjs gnome-core gnome-tweaks libc6-i386 libgl1 libglx0 libglx-mesa0
 
+gsettings set org.gnome.shell.overrides workspaces-only-on-primary false
+
 echo "gnome installed"
