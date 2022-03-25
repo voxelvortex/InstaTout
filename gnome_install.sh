8dbe3402afe4babfbd6cd57958e3c2ba1d0cc6e2
Michael Scalzetti <maestromike5@gmail.com>
2022-03-25 16:55:43 -0400

    added comments

--- gnome_install.sh
+++ gnome_install.sh
@@ -6,6 +6,7 @@ cp ./files/preferences /etc/apt/preferences
 apt-get update &&\
 apt -t unstable install -y gnome-session gnome-shell gnome-backgrounds gnome-applets gnome-control-center mutter gjs gnome-core gnome-tweaks libc6-i386 libgl1 libglx0 libglx-mesa0 power-profiles-daemon
 
+# Make all the monitors swap workspaces together (WHY THE FUCK ISNT THIS DEFAULT GNOME? BRUH.)
 gsettings set org.gnome.mutter workspaces-only-on-primary false
 
 
