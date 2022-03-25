c1447ad9e7cffb328584a9406edceb366d026737
Michael Scalzetti <maestromike5@gmail.com>
2022-03-24 21:05:52 -0400

    added dependency for power profiles

--- gnome_install.sh
+++ gnome_install.sh
@@ -4,7 +4,7 @@ cp ./files/preferences /etc/apt/preferences
 
 # Install gnome
 apt-get update &&\
-apt -t unstable install -y gnome-session gnome-shell gnome-backgrounds gnome-applets gnome-control-center mutter gjs gnome-core gnome-tweaks libc6-i386 libgl1 libglx0 libglx-mesa0
+apt -t unstable install -y gnome-session gnome-shell gnome-backgrounds gnome-applets gnome-control-center mutter gjs gnome-core gnome-tweaks libc6-i386 libgl1 libglx0 libglx-mesa0 power-profiles-daemon
 
 gsettings set org.gnome.shell.overrides workspaces-only-on-primary false
 
